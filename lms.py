98442294523e3cd2bccaeefbe9fe23fe9b995cdb
Yashu_Kohli11 <89150253+yashu1wwww@users.noreply.github.com>
2022-05-11 21:37:16 +0530

    Update lms.py

--- lms.py
+++ lms.py
@@ -4,8 +4,7 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://karnatakalms.com/home")
 
-input=driver.find_element_by_xpath('//*[@id="homebody"]/div[2]/div/button[2]')
-input.click()
+input=driver.find_element_by_xpath('//*[@id="homebody"]/div[2]/div/button[2]').click()
 
 input=driver.find_element_by_xpath('//*[@id="userid"]')
 input.send_keys('ug19snnajaja')
@@ -13,13 +12,12 @@ input.send_keys('ug19snnajaja')
 input=driver.find_element_by_xpath('//*[@id="password"]')
 input.send_keys('11082000')
 
-input=driver.find_element_by_xpath('/html/body/app-root/app-layout-full/div[1]/main/div/app-login/div/div/div/div[2]/div/div[1]/form/div[3]/select')
-input.click()
+input=driver.find_element_by_xpath('/html/body/app-root/app-layout-full/div[1]/main/div/app-login/div/div/div/div[2]/div/div[1]/form/div[3]/select').click()
 
+#currently working on drop handling button so plz select required university and leave it 
 time.sleep(6)
 
-input=driver.find_element_by_xpath('/html/body/app-root/app-layout-full/div[1]/main/div/app-login/div/div/div/div[2]/div/div[1]/form/div[4]/div/button')
-input.click()
+input=driver.find_element_by_xpath('/html/body/app-root/app-layout-full/div[1]/main/div/app-login/div/div/div/div[2]/div/div[1]/form/div[4]/div/button').click()
 
 
 
