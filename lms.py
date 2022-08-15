dcf7aaaa4f2a0a9356651989ba54ab9bf570f46d
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-08-15 22:36:47 +0530

    Update lms.py

--- lms.py
+++ lms.py
@@ -5,12 +5,12 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://karnatakalms.com/home")
 
-input=driver.find_element_by_xpath('//*[@id="homebody"]/div[2]/div/button[2]').click()
+driver.find_element_by_xpath('//*[@id="homebody"]/div[2]/div/button[2]').click()
 
-input=driver.find_element_by_xpath('//*[@id="userid"]')
+driver.find_element_by_xpath('//*[@id="userid"]')
 input.send_keys('ug1......') #enter your user-id
 
-input=driver.find_element_by_xpath('//*[@id="password"]')
+driver.find_element_by_xpath('//*[@id="password"]')
 input.send_keys('dontknow') #enter your password
 
 time.sleep(2)
@@ -36,8 +36,7 @@ dropdown.select_by_value("TU") #I enter tumkur university value select your univ
 #Test University-"ENTMob"
 #New Test University-"KNTU"
 
-
-input=driver.find_element_by_xpath('/html/body/app-root/app-layout-full/div[1]/main/div/app-login/div/div/div/div/div[2]/div/div[1]/form/div[4]/div/button').click()
+driver.find_element_by_xpath('/html/body/app-root/app-layout-full/div[1]/main/div/app-login/div/div/div/div/div[2]/div/div[1]/form/div[4]/div/button').click()
 
 time.sleep(30)
 
